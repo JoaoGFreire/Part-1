22314d78960862c41829a79a020fb8f88c9514d1
PC-JOAO\joaog <joaogfff@gmail.com>
2024-01-31 16:11:50 -0500

    added player movement and collision and walls with collision

---
+++ Assets/Assignment/Scripts/PlayerCube.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCube : MonoBehaviour
+{
+    Rigidbody2D rigibod;
+    public float speed = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigibod = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 force = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed * Time.deltaTime;
+        rigibod.AddForce(force);
+    }
+}
